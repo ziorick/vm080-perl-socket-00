ae9666a86a8081915709d1f6b3ee6cb319272bb4
User Riccardo.ricci <riccardo.ricci@vm-080.vmx.nx>
2016-11-07 12:20:33 +0100

    Corrected TCPServer.pl spacing in lines indent

--- TCPServer.pl
+++ TCPServer.pl
@@ -17,17 +17,17 @@ $socket = new IO::Socket::INET (
 print "Server waiting for clint connection\n";
 
 while(1) {
-   $client_socket = $socket->accept;
-   $peer_address = $client_socket->peerhost();
-   $peer_port = $client_socket->peerport();
+  $client_socket = $socket->accept;
+  $peer_address = $client_socket->peerhost();
+  $peer_port = $client_socket->peerport();
 
-   print "Accepted new client connection from: $peer_address, $peer_port\n";
+  print "Accepted new client connection from: $peer_address, $peer_port\n";
 
-   $data = "Data from server";
-   print $client_socket "$data\n";
+  $data = "Data from server";
+  print $client_socket "$data\n";
 
-   $data = <$client_socket>;
-   print "Received from client: $data\n";
+  $data = <$client_socket>;
+  print "Received from client: $data\n";
 
   $client_socket->close();
 }
